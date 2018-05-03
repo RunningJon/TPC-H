b09246f6a1a4818106156aabd7f86e18cfba63f0
Jon Roberts <jgronline@gmail.com>
2018-05-03 14:00:07 -0400

    1.2.2

--- 08_multi_user_reports/rollout.sh
+++ 08_multi_user_reports/rollout.sh
@@ -24,18 +24,18 @@ for i in $(ls $PWD/*.$filter.*.sql); do
 	echo ""
 done
 
-for i in $(ls $PWD/*.copy.*.sql); do
-	logstep=$(echo $i | awk -F 'copy.' '{print $2}' | awk -F '.' '{print $1}')
-	logfile="$PWD""/../log/rollout_""$logstep"".log"
-	logfile="'""$logfile""'"
-	echo "psql -a -f $i -v LOGFILE=\"$logfile\""
-	psql -a -f $i -v LOGFILE="$logfile"
-	echo ""
+filename=$(ls *.copy.*.sql)
+
+for i in $(ls $PWD/../log/rollout_testing_*); do
+	logfile="'""$i""'"
+	
+        echo "psql -a -f $filename -v LOGFILE=\"$logfile\""
+        psql -a -f $filename -v LOGFILE="$logfile"
 done
 
 psql -t -A -c "select 'analyze ' || n.nspname || '.' || c.relname || ';' from pg_class c join pg_namespace n on n.oid = c.relnamespace and n.nspname = 'tpch_testing'" | psql -t -A -e
 
-psql -F $'\t' -A -P pager=off -f $PWD/detailed_report.sql
+psql -F $'\t' -A -v ON_ERROR_STOP=ON -P pager=off -f $PWD/detailed_report.sql
 echo ""
 
 end_step $step
